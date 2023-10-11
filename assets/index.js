4497b653cab4b49d6a3d54684a0119798f9b5563
Cindrella <cindrellapaul2001@gmail.com>
2023-10-11 16:09:35 +0400

    ui/ux changes

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,9 @@
+// aos js
+AOS.init({
+    duration: 1000,
+    delay: 50,
+  });
+
 // technology used slider
 var copy = document.querySelector(".logos-slide").cloneNode(true);
 document.querySelector(".logo-slider").appendChild(copy);
